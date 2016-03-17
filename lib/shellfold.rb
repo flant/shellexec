cbec1df430f8b4d739fd94ba7727deb0f31e6321
Timofey Kirillov <distorhead@gmail.com>
2016-03-17 16:23:51 +0300

    ++

--- lib/shellfold.rb
+++ lib/shellfold.rb
@@ -64,7 +64,7 @@ module Shellfold
                    "# LAST OUTPUT END\n"].join
             write_out{"\n#{msg}"}
           else
-            write_out{"\n"}
+            write_out{"[IGNORE]\n"}
           end
         else
           write_out{" [DONE]\n"}
