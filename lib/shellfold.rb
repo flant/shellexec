51d8ff898ae1afbfb6463ed2e1ed25571cab347c
Timofey Kirillov <distorhead@gmail.com>
2016-03-17 21:08:46 +0300

    ++

--- lib/shellfold.rb
+++ lib/shellfold.rb
@@ -43,7 +43,7 @@ module Shellfold
       progress_bar_thr = nil
 
       if live_log
-        write_out{[desc_given? ? "#{desc}: " : nil, command.command, "\n"].join}
+        write_out{[desc_given? ? "#{desc} " : nil, "[#{command.command}]", "\n"].compact.join}
       else
         write_out{desc}
         progress_bar_thr = Thread.new do
